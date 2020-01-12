57709f87dd3e87fccef090f02011698aacfe0668
Frankyston Lins <frankyston@gmail.com>
2020-01-12 17:16:15 -0300

    Criando o teste rspec do Model Company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "Company valid" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:code) }
+    it { should validate_presence_of(:token) }
+  end
+
 end
